47c233cea11a2f3592e59ed23ed446210f8c2d91
Tom Jakubowski <tom@crystae.net>
2019-08-27 21:56:52 -0700

    hello glutin

---
+++ src/main.rs
@@ -0,0 +1,30 @@
+use glutin::event::{Event, WindowEvent};
+use glutin::event_loop::{ControlFlow, EventLoop};
+use glutin::window::WindowBuilder;
+use glutin::ContextBuilder;
+
+fn main() {
+    let el = EventLoop::new();
+    let wb = WindowBuilder::new().with_title("shaderforge");
+
+    let windowed_context = ContextBuilder::new().build_windowed(wb, &el).unwrap();
+    let windowed_context = unsafe { windowed_context.make_current().unwrap() };
+    println!(
+        "Pixel format of window's GL context: {:?}",
+        windowed_context.get_pixel_format()
+    );
+
+    el.run(move |event, _, control_flow| {
+        println!("{:?}", event);
+        *control_flow = ControlFlow::Wait;
+
+        match event {
+            Event::LoopDestroyed => return,
+            Event::WindowEvent { ref event, .. } => match event {
+                WindowEvent::CloseRequested => *control_flow = ControlFlow::Exit,
+                _ => (),
+            },
+            _ => (),
+        }
+    });
+}
